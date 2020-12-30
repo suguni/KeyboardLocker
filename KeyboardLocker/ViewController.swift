96d2d0af45ea7361e7b0296cecc5fbcb1e49b81a
Steve Yu <suguni@gmail.com>
2020-12-30 22:02:18 +0900

    Initial Commit

---
+++ KeyboardLocker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KeyboardLocker
+//
+//  Created by suguni on 2020/12/30.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
